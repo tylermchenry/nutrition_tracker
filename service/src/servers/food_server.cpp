17154e3beb3ab4a03b1341bd0ee306e020fc7bdd
Tyler McHenry <tyler@nerdland.net>
2010-07-21 15:28:10 -0700

    Partial implementation of generic food response objects

---
+++ service/src/servers/food_server.cpp
@@ -0,0 +1,63 @@
+/*
+ * food_server.cpp
+ *
+ *  Created on: Jul 21, 2010
+ *      Author: tyler
+ */
+
+#include "servers/food_server.h"
+#include "servers/data_server.h"
+#include "libnutrition/data/food.h"
+#include "libnutrition/proto/data/data.pb.h"
+#include <cassert>
+
+void FoodLoadResponseObjects::addFood
+(const QSharedPointer<Food>& food)
+{
+  if (food && !foodIds.contains(food->getId())) {
+    foodIds.insert(food->getId());
+    foods.append(food);
+  }
+}
+
+void FoodLoadResponseObjects::addFoods
+  (const QVector<QSharedPointer<Food> >& foods)
+{
+  for (QVector<QSharedPointer<Food> >::const_iterator i = foods.begin();
+       i != foods.end(); ++i)
+  {
+    addFood(*i);
+  }
+}
+
+void FoodLoadResponseObjects::addFoods
+  (const QList<QSharedPointer<Food> >& foods)
+{
+  for (QList<QSharedPointer<Food> >::const_iterator i = foods.begin();
+       i != foods.end(); ++i)
+  {
+    addFood(*i);
+  }
+}
+
+QList<QSharedPointer<Food> > FoodLoadResponseObjects::getFoods() const
+{
+  return foods;
+}
+
+DataLoadResponse& FoodLoadResponseObjects::serialize(DataLoadResponse& resp) const
+{
+  FoodData fdata;
+
+  for (QList<QSharedPointer<Food> >::const_iterator i = foods.begin();
+       i != foods.end(); ++i)
+  {
+    (*i)->serialize(fdata);
+  }
+
+  // TODO: See if there is a better way to do this copying
+
+  assert(fdata.meals_size() == 0);
+
+  return resp;
+}
---
+++ service/src/servers/food_server.h
@@ -0,0 +1,38 @@
+/*
+ * food_server.h
+ *
+ *  Created on: Jul 21, 2010
+ *      Author: tyler
+ */
+
+#ifndef FOOD_SERVER_H_
+#define FOOD_SERVER_H_
+
+#include "libnutrition/data/food.h"
+#include <QString>
+#include <QSet>
+
+class DataLoadResponse; // forward decl
+
+class FoodLoadResponseObjects
+{
+  public:
+
+    void addFood(const QSharedPointer<Food>& food);
+    void addFoods(const QVector<QSharedPointer<Food> >& foods);
+    void addFoods(const QList<QSharedPointer<Food> >& foods);
+
+    inline bool isEmpty() const { return foods.isEmpty(); }
+
+    QList<QSharedPointer<Food> > getFoods() const;
+
+    DataLoadResponse& serialize(DataLoadResponse& resp) const;
+
+  private:
+
+    QSet<QString> foodIds;
+    QList<QSharedPointer<Food> > foods;
+};
+
+
+#endif /* FOOD_SERVER_H_ */
